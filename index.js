1c0c04f751f6e25e6a43f2177f38d991a196918c
ArtenCool <artem04102021@gmail.com>
2024-06-18 12:02:24 +0300

    create models

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import { User, Book, Token } from './models/models.js'
+import sequelise from './db/db.js';
 import express from 'express';
 import cors from 'cors';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL]
+}));
+app.use(cookieParser());
+app.use(express.json());
 
 const startApp = async () => {
     try {
+        await sequelise.authenticate();
+        await sequelise.sync();
         app.listen(PORT, () => console.log(`Started a ${PORT} server`));
     } catch (err) {
         console.log(err);
---
+++ models/models.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db/db.js";
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING(40), unique: true },
+    username: { type: DataTypes.STRING(30) },
+    password: { type: DataTypes.STRING(120) },
+    role: { type: DataTypes.STRING(10), defaultValue: "USER" },
+});
+
+const Book = sequelize.define('book', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING(80) },
+    author: { type: DataTypes.STRING(40) },
+    genres: { type: DataTypes.STRING(40) },
+    user_id: { type: DataTypes.INTEGER },
+});
+
+const Token = sequelize.define('token', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    user_id: { type: DataTypes.INTEGER },
+    refreshToken: { type: DataTypes.STRING },
+});
+
+User.hasMany(Book);
+Book.belongsTo(User);
+
+User.hasOne(Token);
+Token.belongsTo(User);
+
+export { User, Book, Token };
